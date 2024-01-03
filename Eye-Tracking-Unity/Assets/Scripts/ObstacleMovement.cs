9ac301044bbc4ccab59df326613b599f556fe851
Jhundon De Leon Mendi <jhundon.mendi21@gmail.com>
2024-01-03 17:44:36 +0000

    Worked on Segment Prefab

---
+++ Eye-Tracking-Unity/Assets/Scripts/ObstacleMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] float speed = 2f;
+    private const float UnityUnitsPerMeter = 1f;
+
+    void Update()
+    {
+        Move();
+
+        // Destroys the object once it passes the playzone lower bounds
+        if (gameObject.transform.position.x > 40)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Move()
+    {
+        // Convert speed from meters per second to Unity units per second
+        float speedInUnityUnitsPerSecond = speed / UnityUnitsPerMeter;
+        // Move the object down the lane
+        transform.Translate(Vector3.forward * speedInUnityUnitsPerSecond * Time.deltaTime);
+    }
+}
