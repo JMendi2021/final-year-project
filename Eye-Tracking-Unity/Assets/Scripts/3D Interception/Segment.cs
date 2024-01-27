99b6001325833e37586ddbf4615edc9c8a780f35
Jhundon De Leon Mendi <jhundon.mendi21@gmail.com>
2024-01-27 04:03:00 +0000

    Created 3D Interception Scene with recreated assets

---
+++ Eye-Tracking-Unity/Assets/Scripts/3D Interception/Segment.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Segment : MonoBehaviour
+{
+    [Header("Spawn Settings")]
+    [SerializeField] GameObject spawner;
+
+}
